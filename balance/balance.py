ae225f39ceba490615ba97b64b334cedf2c420a4
ryan <wrathofhorde@gmail.com>
2024-05-19 00:13:27 +0900

    change to use commonly

--- balance/balance.py
+++ balance/balance.py
@@ -3,10 +3,8 @@ import owns
 
 # with open("stocks.json") as s:
 #   stocks = json.load(s)
-
 # snp = stocks[0];
 # print(json.dumps(snp, indent=2, ensure_ascii=False))
-
 
 sum = 0
 total_ratio = 0
@@ -29,10 +27,6 @@ for s in stocks:
   s[keys.ASSIGNMENT] = s[keys.ASSIGNMENT] if s[keys.ASSIGNMENT] >= s[keys.UNIT_PRICE] else 0
   s[keys.BUY] = s[keys.ASSIGNMENT] // s[keys.UNIT_PRICE]
 
-snp = stocks[index[keys.SNP]]
-qqq = stocks[index[keys.QQQ]]
-n50 = stocks[index[keys.NIFTY_50]]
-
 total_price_to_buy = 0
 
 for s in stocks:
@@ -40,10 +34,11 @@ for s in stocks:
 
 balance = deposit - total_price_to_buy
 
-if balance > n50[keys.UNIT_PRICE]:
-  extra_buy = balance // n50[keys.UNIT_PRICE]
-  n50[keys.BUY] += extra_buy
-  balance -= extra_buy * n50[keys.UNIT_PRICE]
+for s in stocks:
+  if s[keys.BUY] == 0 and balance > s[keys.UNIT_PRICE]:
+    extra_buy = balance // s[keys.UNIT_PRICE]
+    s[keys.BUY] += extra_buy
+    balance -= extra_buy * s[keys.UNIT_PRICE]
 
 total_balance = 0
 
@@ -51,16 +46,11 @@ for s in stocks:
   s[keys.BALANCE_AFTER_BUY] = s[keys.BUY] * s[keys.UNIT_PRICE] + s[keys.TOTAL_PRICE]
   total_balance += s[keys.BALANCE_AFTER_BUY]
 
-snp_ratio = round(100 * snp[keys.BALANCE_AFTER_BUY] / total_balance, 1)
-qqq_ratio = round(100 * qqq[keys.BALANCE_AFTER_BUY] / total_balance, 1)
-n50_ratio = round(100 * n50[keys.BALANCE_AFTER_BUY] / total_balance, 1)
+for s in stocks:
+  s[keys.RATIO_AFTER_BUY] = round(100 * s[keys.BALANCE_AFTER_BUY] / total_balance, 1)
+  print(s)
 
-print(snp)
-print(qqq)
-print(n50)
-
-print(f"{snp[keys.NAME]}: {snp[keys.BUY]}, {snp_ratio}%")
-print(f"{qqq[keys.NAME]}: {qqq[keys.BUY]}, {qqq_ratio}%")
-print(f"{n50[keys.NAME]}: {n50[keys.BUY]}, {n50_ratio}%")
+for s in stocks:
+  print(f"{s[keys.NAME]}: {s[keys.BUY]}, {s[keys.RATIO_AFTER_BUY]}%")
 
 print(f"잔액: {balance}")

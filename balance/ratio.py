3f44213d44df30001717738010dec10db0f3d3da
ryan <wrathofhorde@gmail.com>
2024-05-20 23:41:03 +0900

    add sort

--- balance/ratio.py
+++ balance/ratio.py
@@ -1,9 +1,10 @@
 import sys
 import json
 import keys
+from operator import itemgetter
 import matplotlib.pyplot as plot
 
-PRECISION = 1
+PRECISION = 2
 
 if __name__ == "__main__":
   if len(sys.argv) != 2:
@@ -21,6 +22,8 @@ for s in stocks:
   s[keys.TOTAL_PRICE] = s[keys.QUANTITY] * s[keys.UNIT_PRICE]
   total_balance += s[keys.TOTAL_PRICE]
 
+stocks = sorted(stocks, key=itemgetter(keys.TOTAL_PRICE), reverse=True)
+
 ratio = []
 labels = []
 
@@ -33,7 +36,7 @@ with open("ratio_out.json", 'w', encoding="utf-8") as w:
   json.dump(stocks, w, ensure_ascii = False, indent = 2)
   
 wedgeprops={'width': 0.7, 'edgecolor': 'w', 'linewidth': 5}
-plot.rc("font", size=16)
-plot.pie(ratio, labels=labels, autopct="%.1f%%", wedgeprops=wedgeprops)
+plot.rc("font", size=12)
+plot.pie(ratio, labels=labels, autopct="%.2f%%", wedgeprops=wedgeprops)
 plot.show()
 

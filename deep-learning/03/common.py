e009bfe5fe7503d0ed13f3e7779c9d281abd179e
wrath <wrathofhorde@gmail.com>
2024-06-24 16:06:43 +0900

    add gitignore

--- deep-learning/03/common.py
+++ deep-learning/03/common.py
@@ -1,11 +1,10 @@
-import step
-import sigmoid
+import func
 import numpy as np
 import matplotlib.pylab as plt
 
 x = np.arange(-5, 5, 0.1)
-y1 = step.step_function(x)
-y2 = sigmoid.sigmoid(x)
+y1 = func.step(x)
+y2 = func.sigmoid(x)
 
 plt.plot(x, y1)
 plt.plot(x, y2)

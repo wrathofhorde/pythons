d97e59dec1a73d8249ff55b3dc86bfd6b0582943
wrath <wrathofhorde@gmail.com>
2024-08-05 16:28:24 +0900

    add functions to list

--- linkedlist/list2.py
+++ linkedlist/list2.py
@@ -1,19 +1,30 @@
-class Node:
-    def __init__(self, num):
-        self.data = num
-        self.next = None
+from icecream import ic
 
+# ic.disable()
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 class List:
     def __init__(self):
+        self.len = 0
         self.head = Node(0)
         self.tail = Node(0)
         self.head.next = self.tail
 
-    def insert(self, num):
-        node = Node(num)
+    def is_empty(self):
+        return self.head.next is self.tail
+
+    def insert(self, data):
+        node = Node(data)
         node.next = self.head.next
         self.head.next = node
+        self.len += 1
+
+        ic(f"{data} inserted, size:{self.len}")
+
         return node
 
     def print(self):
@@ -22,26 +33,61 @@ class List:
             print(curr.data)
             curr = curr.next
 
-    def find(self, num):
+    def find(self, data):
         curr = self.head.next
         while curr is not self.tail:
-            if curr.data == num:
+            if curr.data is data:
                 return curr
             curr = curr.next
 
         return None
+    
+    def delete(self, data):
+        prev = self.head
+        curr = self.head.next
 
+        while curr is not self.tail:
+            if curr.data is data:
+                self.len -= 1
+                prev.next = curr.next
 
-list = List()
-list.insert(1)
-list.insert(2)
-list.insert(3)
-list.insert(4)
-list.print()
+                ic(f"{data} deleted, size:{self.len}")
+                return True
+            else:
+                prev = curr
+                curr = curr.next
 
-ret = list.find(3)
+        ic(f"{data} is not found")
+        return False
 
-if ret is not None:
-    print(ret.data)
-else:
-    print("not found")
+    def add(self, data):
+        prev = self.head
+        curr = self.head.next
+
+        while curr is not self.tail:
+            prev = curr
+            curr = curr.next
+
+        node = Node(data)
+        prev.next = node
+        node.next = curr
+        self.len += 1
+        ic(f"{data} added, size:{self.len}")
+
+    def size(self):
+        ic(f"length of list:{self.len}")
+        return self.len
+
+if __name__ == "__main__":
+    list = List()
+    list.add(1)
+    list.insert(2)
+    list.insert(3)
+    list.add(4)
+    list.print()
+    print(list.delete(5))
+    list.add(6)
+    print(list.delete(3))
+    print(list.delete(1))
+    list.print()
+    list.size()
